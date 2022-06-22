2ef5ed76da3c8920b9bd0c09643b16fd36617643
RumShot <sotasromas@gmail.com>
2022-06-22 17:08:02 +0300

    +logging, ELKO check POST response

--- order_sender/sender/playground.py
+++ order_sender/sender/playground.py
@@ -9,6 +9,17 @@ import logging
 today = datetime.now()
 date_now = today.strftime('%y%m%d_%H_%M')
 
+log_date = today.strftime('%y%m')
+logger = logging.getLogger(__name__)
+logger_file = logging.FileHandler('order_sender/sender/logging/error' + log_date + '.log')
+logger.addHandler(logger_file)
+logger.setLevel(logging.INFO)
+logger_formater = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(module)s:%(lineno)s:%(message)s')
+logger_file.setFormatter(logger_formater)
+streamer_handler = logging.StreamHandler()
+streamer_handler.setFormatter(logger_formater)
+logger.addHandler(streamer_handler)
+
 def database_connection(sql_command):
     try:
         conn = mariadb.connect(
@@ -26,6 +37,7 @@ def database_connection(sql_command):
         return command_return
 
     except mariadb.Error as e:
+        logger.info(f"couldn't connect: {e}")
         message = f"Error connecting to the database: {e}"
 
         return message
@@ -69,7 +81,7 @@ def elko_generate_order():
                     "apartment": "",
                     "isNaturalPerson": "true",
                     "customerPo": "string",
-                    "contactMail": "" + elko_email + "",
+                    "contactMail": "string" + elko_email + "",
                     "pickupPoint": 0
                 },
                 "created": 0,
@@ -101,17 +113,17 @@ def elko_generate_order():
                 "id": "" + elko_product_id + ""
                 }
             # save_json(distributor_message, file_name)
-            # elko_post_request(distributor_message)
+            elko_post_request(distributor_message, order_id)
             # elko_change_status(order_id)
     else:
-        print("Elko no data received")
+        logger.info(f"Request have been made to ELKO. No data from SQL came back for order_status_id = 2")
 
 # JSON save 
 def save_json(distributor_message, file_name):
     with open('order_sender/sender/order_messages/' + file_name, 'w+', encoding="utf-8") as json_file:
         json.dump(distributor_message,json_file)   
 
-def elko_post_request(distributor_message):
+def elko_post_request(distributor_message, order_id):
     bearer = open("order_sender/sender/authentications/elko_bearer.txt", "r")
     url = "https://api.elko.cloud/v3.0/api/Orders"
     payload = json.dumps(distributor_message)
@@ -119,14 +131,21 @@ def elko_post_request(distributor_message):
                 'Content-Type': 'application/json'
                 }
     response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
+
+    try:
+        json.loads(response.text)
+        file_name_resp = order_id+ '_ELKO_' + date_now + '_response.json'
+        with open('order_sender/sender/order_messages/' + file_name_resp, 'w+', encoding="utf-8") as json_response:
+            json.dump(response.text, json_response)
+    except ValueError as e:
+        logger.info(f"Order of ID: { order_id } received error from ELKO: {response.text}")
 
 # changing order_status_id from Processing --> Shipping
 def elko_change_status(order_id):
     sql_command = "UPDATE `svst_order` SET order_status_id = 3 WHERE order_id = " + order_id
     database_connection(sql_command)
 
-# elko_generate_order()
+elko_generate_order()
 
 
 # ------------ ACC --------------------
@@ -157,12 +176,12 @@ def acc_generate_order():
             acc_user_comment = sigle_data_received[44]
             # TEST LICENCE
             licence_key = acc_test_lc.licence
-            distributor_message = "{\r\n\"request\": {\r\n\"CompanyId\": \"_al\",\r\n\"LicenseKey\": \"" + str(licence_key) + "\",\r\n\"OrderInfo\": {\r\n\"DlvPointId\": \"305681192\",\r\n\"DlvChannel\": \"20\",\r\n\"DlvType\": \"1\",\r\n\"DlvFirstName\": \"" + acc_name + "\",\r\n\"DlvLastName\": \"" + acc_surname + "\",\r\n\"DlvCountryId\": \"" + acc_country_id + "\",\r\n\"DlvCity\": \"" + acc_shipping_city + "\",\r\n\"DlvStreet\": \"" + acc_shipping_address+ "\",\r\n\"DlvZipCode\": \"" + acc_shipping_postcode + "\",\r\n\"DlvEmail\": \"" + acc_email + "\",\r\n\"DlvPhone\": \"" + acc_telephone + "\",\r\n\"DlvCarrierNote\": \"" + acc_user_comment + "\",\r\n\"SplitDestination\": \"10\",\r\n\"SplitDelivery\": \"10\"\r\n},\r\n\"OrderLines\": [{\r\n\"ProductId\": \"320848\",\r\n\"Quantity\": 1\r\n}]\r\n}\r\n}"
+            distributor_message = "{\r\n\"request\": {\r\n\"CompanyId\": \"_al\",\r\n\"LicenseKey\": \"" + str(licence_key) + "\",\r\n\"OrderInfo\": {\r\n\"DlvPointId\": \"305681192\",\r\n\"DlvChannel\": \"20\",\r\n\"DlvType\": \"1\",\r\n\"DlvFirstName\": \"" + acc_name + "\",\r\n\"DlvLastName\": \"" + acc_surname + "\",\r\n\"DlvCountryId\": \"" + acc_country_id + "\",\r\n\"DlvCity\": \"" + acc_shipping_city + "\",\r\n\"DlvStreet\": \"" + acc_shipping_address+ "\",\r\n\"DlvZipCode\": \"" + acc_shipping_postcode + "\",\r\n\"DlvEmail\": \"" + acc_email + "\",\r\n\"DlvPhone\": \"" + acc_telephone + "\",\r\n\"DlvCarrierNote\": \"" + acc_user_comment + "\",\r\n\"SplitDestination\": \"10\",\r\n\"SplitDelivery\": \"10\"\r\n},\r\n\"OrderLines\": [{\r\n\"ProductId\": \"" + acc_product_id + "\",\r\n\"Quantity\": " + acc_quantity + "\r\n}]\r\n}\r\n}"
             save_json(distributor_message, file_name)
             acc_post_request(distributor_message, order_id)
             # acc_change_status(order_id, order_id)
     else:
-        print("ACC no data received")
+        logger.info(f"Request have been made to ACC. No data from SQL came back for order_status_id = 2")
 
 def acc_post_request(distributor_message, order_id):
     # TEST DISTRIBUTION !!!!!!!!!!!!
@@ -172,7 +191,7 @@ def acc_post_request(distributor_message, order_id):
     }
     response = requests.request("POST", acc_url, headers=headers, data=distributor_message.encode())
     print(response.text)
-    file_name_resp = order_id+ '_ACC_' + date_now + 'response.json'
+    file_name_resp = order_id+ '_ACC_' + date_now + '_response.json'
     with open('order_sender/sender/order_messages/' + file_name_resp, 'w+', encoding="utf-8") as json_file:
         json.dump(response.text, json_file)   
 
@@ -180,4 +199,4 @@ def acc_change_status(order_id):
     sql_command = "UPDATE `svst_order` SET order_status_id = 3 WHERE order_id = " + order_id
     database_connection(sql_command)
 
-acc_generate_order()
+# acc_generate_order()
