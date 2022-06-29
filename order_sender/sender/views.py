f78263fc242b612ef4d41e42a183a26c99400293
RumShot <sotasromas@gmail.com>
2022-06-29 09:04:52 +0300

    code fix

--- order_sender/sender/views.py
+++ order_sender/sender/views.py
@@ -1,196 +1,125 @@
 from django.shortcuts import render
 import mariadb
-from django.contrib import messages
 from os.path import join
 import os
+from django.core.paginator import Paginator
 
+
+def database_connection(sql_command):
+    try:
+        conn = mariadb.connect(
+            host="127.0.0.1",
+            port=3307,
+            user="root",
+            password="",
+            database="ptu1_data_base"
+            )
+        cursor  = conn.cursor()
+        cursor.execute(sql_command)
+        command_return = cursor.fetchall()
+        conn.close()
+        is_connected = 1
+
+        return command_return, is_connected
+
+    except mariadb.Error as e:
+        is_connected = 0
+        messages = f"Error connecting to the database: {e}"
+
+        return messages, is_connected
 
 def data_base(request):
-    # conection to MariaDB
-    try:
-        conn = mariadb.connect(
-            host="127.0.0.1",
-            port=3307,
-            user="root",
-            password="",
-            database="ptu1_data_base"
-            )
-        is_connected = 1
-    except mariadb.Error as e:
-        is_connected = 0
-        messages.error(request, f"Error connecting to the database: {e}")
-        return render(request, 'index.html')
+    sql_command = "SELECT COUNT(order_id) FROM svst_order"
+    overall_orders = database_connection(sql_command)
 
-    if is_connected == 1:
-        # overall orders
-        cursor  = conn.cursor() 
-        cursor.execute("SELECT COUNT(order_id) FROM svst_order")
-        overall_orders = cursor.fetchall()
-        # unique distributors
-        cursor.execute("SELECT DISTINCT import_batch FROM svst_product")
-        distributors = cursor.fetchall()
-        distrubutor_list = []
+    sql_command = "SELECT DISTINCT import_batch FROM svst_product"
+    distributors = database_connection(sql_command)
+    inner_dist = distributors[0]
+    distrubutor_list = []
 
-        for distributor in distributors:
-            fixed_dist = distributor[0].split()[0].replace('LT', '').replace('LV', '')
-            distrubutor_list.append(fixed_dist) if fixed_dist not in distrubutor_list else distrubutor_list
-        # processing count
-        cursor.execute("SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 2")
-        processing_count = cursor.fetchall()
+    for distributor in inner_dist:
+        fixed_dist = distributor[0].replace('LT', '').replace('LV', '').split()[0]
+        distrubutor_list.append(fixed_dist) if fixed_dist.split()[0] not in distrubutor_list else distrubutor_list
 
-        cursor.execute("SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 3")
-        shipped_count = cursor.fetchall()
+    sql_command = "SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 2"
+    processing_count = database_connection(sql_command)
 
-        cursor.execute("SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 5")
-        completed_count = cursor.fetchall()
+    sql_command = "SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 3"
+    shipped_count = database_connection(sql_command)
 
-        cursor.execute("SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id NOT IN (2, 3, 5)")
-        unprocessed_count = cursor.fetchall()
+    sql_command = "SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 5"
+    completed_count = database_connection(sql_command)
 
-        context = {
-            'overall_orders': overall_orders,
-            'is_connected': int(is_connected),
-            'distributors': distrubutor_list,
-            'processing_count': processing_count,
-            'shipped_count': shipped_count,
-            'completed_count': completed_count,
-            'unprocessed_count': unprocessed_count,
+    sql_command = "SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id NOT IN (2, 3, 5)"
+    unprocessed_count = database_connection(sql_command)
+
+    context = {
+        'overall_orders': overall_orders[0],
+        'distributors': distrubutor_list,
+        'processing_count': processing_count[0],
+        'shipped_count': shipped_count[0],
+        'completed_count': completed_count[0],
+        'unprocessed_count': unprocessed_count[0],
         }
 
-        conn.close()
-        return render(request, 'index.html', context)
+    return render(request, 'index.html', context)
+
 
 def processing_list(request):
-    try:
-        conn = mariadb.connect(
-            host="127.0.0.1",
-            port=3307,
-            user="root",
-            password="",
-            database="ptu1_data_base"
-            )
-        is_connected = 1
-    except mariadb.Error as e:
-        is_connected = 0
-        messages.error(request, f"Error connecting to the database: {e}")
-        return render(request, 'index.html')
+    sql_command = "SELECT * FROM `svst_order` WHERE order_status_id = 2 ORDER BY date_added DESC"
+    overall_processing = database_connection(sql_command)
 
-    if is_connected == 1:
-        cursor  = conn.cursor() 
-        cursor.execute("SELECT * FROM `svst_order` WHERE order_status_id = 2 ORDER BY date_added DESC")
-        overall_processing = cursor.fetchall()
+    paginator = Paginator(overall_processing[0], 3)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
 
-        context = {
-            'overall_processing': overall_processing,
-            'is_connected': int(is_connected),
+    context = {
+        'overall_processing': overall_processing[0],
+        'page': page,
         }
 
-        conn.close()
-        return render(request, 'processing.html', context)
+    return render(request, 'processing.html', context)
+
 
 def shipped_list(request):
-    try:
-        conn = mariadb.connect(
-            host="127.0.0.1",
-            port=3307,
-            user="root",
-            password="",
-            database="ptu1_data_base"
-            )
-        is_connected = 1
-    except mariadb.Error as e:
-        is_connected = 0
-        messages.error(request, f"Error connecting to the database: {e}")
-        return render(request, 'index.html')
+    sql_command = "SELECT * FROM `svst_order` WHERE order_status_id = 3 ORDER BY date_added DESC"
+    overall_shipped = database_connection(sql_command)
 
-    if is_connected == 1:
-        cursor  = conn.cursor() 
-        cursor.execute("SELECT * FROM `svst_order` WHERE order_status_id = 3 ORDER BY date_added DESC")
-        overall_processing = cursor.fetchall()
-
-        context = {
-            'overall_processing': overall_processing,
-            'is_connected': int(is_connected),
+    context = {
+        'overall_shipped': overall_shipped[0],
         }
 
-        conn.close()
-        return render(request, 'processing.html', context)
+    return render(request, 'shipped.html', context)
 
 
 def completed_list(request):
-    try:
-        conn = mariadb.connect(
-            host="127.0.0.1",
-            port=3307,
-            user="root",
-            password="",
-            database="ptu1_data_base"
-            )
-        is_connected = 1
-    except mariadb.Error as e:
-        is_connected = 0
-        messages.error(request, f"Error connecting to the database: {e}")
-        return render(request, 'index.html')
+    sql_command = "SELECT * FROM `svst_order` WHERE order_status_id = 5 ORDER BY date_added DESC"
+    overall_complete = database_connection(sql_command)
 
-    if is_connected == 1:
-        cursor  = conn.cursor() 
-        cursor.execute("SELECT * FROM `svst_order` WHERE order_status_id = 5 ORDER BY date_added DESC")
-        overall_processing = cursor.fetchall()
-
-        context = {
-            'overall_processing': overall_processing,
-            'is_connected': int(is_connected),
+    context = {
+        'overall_complete': overall_complete[0],
         }
 
-        conn.close()
-        return render(request, 'completed.html', context)
+    return render(request, 'completed.html', context)
+
 
 def hole_list(request):
-    try:
-        conn = mariadb.connect(
-            host="127.0.0.1",
-            port=3307,
-            user="root",
-            password="",
-            database="ptu1_data_base"
-            )
-        is_connected = 1
-    except mariadb.Error as e:
-        is_connected = 0
-        messages.error(request, f"Error connecting to the database: {e}")
-        return render(request, 'index.html')
+    sql_command = "SELECT * FROM `svst_order` WHERE order_status_id NOT IN (2, 3, 5) ORDER BY date_added DESC"
+    overall_hole = database_connection(sql_command)
 
-    if is_connected == 1:
-        cursor = conn.cursor() 
-        cursor.execute("SELECT * FROM `svst_order` WHERE order_status_id NOT IN (2, 3, 5) ORDER BY date_added DESC")
-        overall_processing = cursor.fetchall()
-
-        context = {
-            'overall_processing': overall_processing,
-            'is_connected': int(is_connected),
+    context = {
+        'overall_hole': overall_hole[0],
         }
 
-        conn.close()
-        return render(request, 'hole.html', context)
+    return render(request, 'hole.html', context)
+
 
 def log_list(request):
-    try:
-        conn = mariadb.connect(
-            host="127.0.0.1",
-            port=3307,
-            user="root",
-            password="",
-            database="ptu1_data_base"
-            )
-        is_connected = 1
-    except mariadb.Error as e:
-        is_connected = 0
-        messages.error(request, f"Error connecting to the database: {e}")
-        return render(request, 'logs.html')
-
     logs = 'sender/static/'
     files = os.listdir(join(logs, 'logs'))
-    context = {'files': [join('logs/', file) for file in files],
-               'is_connected': int(is_connected), 
-                }
+
+    context = {
+        'files': [join('logs/', file) for file in files],
+        }
+
     return render(request, 'logs.html', context)
---
+++ order_sender/sender/views_old.py
@@ -0,0 +1,196 @@
+from django.shortcuts import render
+import mariadb
+from django.contrib import messages
+from os.path import join
+import os
+
+
+def data_base(request):
+    # conection to MariaDB
+    try:
+        conn = mariadb.connect(
+            host="127.0.0.1",
+            port=3307,
+            user="root",
+            password="",
+            database="ptu1_data_base"
+            )
+        is_connected = 1
+    except mariadb.Error as e:
+        is_connected = 0
+        messages.error(request, f"Error connecting to the database: {e}")
+        return render(request, 'index.html')
+
+    if is_connected == 1:
+        # overall orders
+        cursor  = conn.cursor() 
+        cursor.execute("SELECT COUNT(order_id) FROM svst_order")
+        overall_orders = cursor.fetchall()
+        # unique distributors
+        cursor.execute("SELECT DISTINCT import_batch FROM svst_product")
+        distributors = cursor.fetchall()
+        distrubutor_list = []
+
+        for distributor in distributors:
+            fixed_dist = distributor[0].split()[0].replace('LT', '').replace('LV', '')
+            distrubutor_list.append(fixed_dist) if fixed_dist not in distrubutor_list else distrubutor_list
+        # processing count
+        cursor.execute("SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 2")
+        processing_count = cursor.fetchall()
+
+        cursor.execute("SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 3")
+        shipped_count = cursor.fetchall()
+
+        cursor.execute("SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id = 5")
+        completed_count = cursor.fetchall()
+
+        cursor.execute("SELECT COUNT(order_status_id) FROM svst_order WHERE order_status_id NOT IN (2, 3, 5)")
+        unprocessed_count = cursor.fetchall()
+
+        context = {
+            'overall_orders': overall_orders,
+            'is_connected': int(is_connected),
+            'distributors': distrubutor_list,
+            'processing_count': processing_count,
+            'shipped_count': shipped_count,
+            'completed_count': completed_count,
+            'unprocessed_count': unprocessed_count,
+        }
+
+        conn.close()
+        return render(request, 'index.html', context)
+
+def processing_list(request):
+    try:
+        conn = mariadb.connect(
+            host="127.0.0.1",
+            port=3307,
+            user="root",
+            password="",
+            database="ptu1_data_base"
+            )
+        is_connected = 1
+    except mariadb.Error as e:
+        is_connected = 0
+        messages.error(request, f"Error connecting to the database: {e}")
+        return render(request, 'index.html')
+
+    if is_connected == 1:
+        cursor  = conn.cursor() 
+        cursor.execute("SELECT * FROM `svst_order` WHERE order_status_id = 2 ORDER BY date_added DESC")
+        overall_processing = cursor.fetchall()
+
+        context = {
+            'overall_processing': overall_processing,
+            'is_connected': int(is_connected),
+        }
+
+        conn.close()
+        return render(request, 'processing.html', context)
+
+def shipped_list(request):
+    try:
+        conn = mariadb.connect(
+            host="127.0.0.1",
+            port=3307,
+            user="root",
+            password="",
+            database="ptu1_data_base"
+            )
+        is_connected = 1
+    except mariadb.Error as e:
+        is_connected = 0
+        messages.error(request, f"Error connecting to the database: {e}")
+        return render(request, 'index.html')
+
+    if is_connected == 1:
+        cursor  = conn.cursor() 
+        cursor.execute("SELECT * FROM `svst_order` WHERE order_status_id = 3 ORDER BY date_added DESC")
+        overall_shipped = cursor.fetchall()
+
+        context = {
+            'overall_shipped': overall_shipped,
+            'is_connected': int(is_connected),
+        }
+
+        conn.close()
+        return render(request, 'processing.html', context)
+
+
+def completed_list(request):
+    try:
+        conn = mariadb.connect(
+            host="127.0.0.1",
+            port=3307,
+            user="root",
+            password="",
+            database="ptu1_data_base"
+            )
+        is_connected = 1
+    except mariadb.Error as e:
+        is_connected = 0
+        messages.error(request, f"Error connecting to the database: {e}")
+        return render(request, 'index.html')
+
+    if is_connected == 1:
+        cursor  = conn.cursor() 
+        cursor.execute("SELECT * FROM `svst_order` WHERE order_status_id = 5 ORDER BY date_added DESC")
+        overall_complete = cursor.fetchall()
+
+        context = {
+            'overall_complete': overall_complete,
+            'is_connected': int(is_connected),
+        }
+
+        conn.close()
+        return render(request, 'completed.html', context)
+
+def hole_list(request):
+    try:
+        conn = mariadb.connect(
+            host="127.0.0.1",
+            port=3307,
+            user="root",
+            password="",
+            database="ptu1_data_base"
+            )
+        is_connected = 1
+    except mariadb.Error as e:
+        is_connected = 0
+        messages.error(request, f"Error connecting to the database: {e}")
+        return render(request, 'index.html')
+
+    if is_connected == 1:
+        cursor = conn.cursor() 
+        cursor.execute("SELECT * FROM `svst_order` WHERE order_status_id NOT IN (2, 3, 5) ORDER BY date_added DESC")
+        overall_hole = cursor.fetchall()
+
+        context = {
+            'overall_hole': overall_hole,
+            'is_connected': int(is_connected),
+        }
+
+        conn.close()
+        return render(request, 'hole.html', context)
+
+def log_list(request):
+    try:
+        conn = mariadb.connect(
+            host="127.0.0.1",
+            port=3307,
+            user="root",
+            password="",
+            database="ptu1_data_base"
+            )
+        is_connected = 1
+    except mariadb.Error as e:
+        is_connected = 0
+        messages.error(request, f"Error connecting to the database: {e}")
+        return render(request, 'logs.html')
+
+    logs = 'sender/static/'
+    files = os.listdir(join(logs, 'logs'))
+    context = {'files': [join('logs/', file) for file in files],
+               'is_connected': int(is_connected), 
+                }
+    return render(request, 'logs.html', context)
